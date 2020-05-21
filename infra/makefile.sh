283d447513ce1cdbc972bbb926c36e36475e373c
Lucas Ko <lucas.ko@dotmatics.com>
2020-05-21 12:24:01 +0100

    added cpus

--- infra/makefile.sh
+++ infra/makefile.sh
@@ -14,15 +14,23 @@ function stack_deploy_browser(){
 
 
     if [ "$BIOREGISTER_ENABLE" = "true" ] &&  [ "$VORTEX_ENABLE" = "true" ]  ; then
-        BROWSER_LIMIT_MEM=$MEM_35_PCT  BIOREGISTER_LIMIT_MEM=$MEM_35_PCT  VORTEX_LIMIT_MEM=$MEM_10_PCT  TRAEFIK_LIMIT_MEM=$MEM_05_PCT   REDIRECT_LIMIT_MEM=$MEM_05_PCT    STACK_NAME=$BRO_STACK_NAME ENV=$ENV docker stack deploy --with-registry-auth  -c docker/stack/browser/browser.yml -c docker/stack/browser/browser-$ENV.yml $BRO_STACK_NAME
+        BROWSER_MEM=$MEM_35_PCT  BIOREGISTER_MEM=$MEM_35_PCT  VORTEX_MEM=$MEM_10_PCT   TRAEFIK_MEM=$MEM_05_PCT  REDIRECT_MEM=$MEM_05_PCT  \
+        BROWSER_CPU=$CPU_35_PCT  BIOREGISTER_CPU=$CPU_35_PCT  VORTEX_CPU=$CPU_10_PCT   TRAEFIK_CPU=$CPU_05_PCT  REDIRECT_MEM=$MEM_05_PCT   STACK_NAME=$BRO_STACK_NAME ENV=$ENV docker stack deploy --with-registry-auth  -c docker/stack/browser/browser.yml -c docker/stack/browser/browser-$ENV.yml $BRO_STACK_NAME
+
 
     elif [ "$BIOREGISTER_ENABLE" = "true" ] &&  [ "$VORTEX_ENABLE" = "false" ]  ; then
-        BROWSER_LIMIT_MEM=$MEM_50_PCT  BIOREGISTER_LIMIT_MEM=$MEM_30_PCT  VORTEX_LIMIT_MEM=$MEM_00_PCT  TRAEFIK_LIMIT_MEM=$MEM_05_PCT   REDIRECT_LIMIT_MEM=$MEM_05_PCT    STACK_NAME=$BRO_STACK_NAME ENV=$ENV docker stack deploy --with-registry-auth  -c docker/stack/browser/browser.yml -c docker/stack/browser/browser-$ENV.yml $BRO_STACK_NAME
+        BROWSER_MEM=$MEM_50_PCT  BIOREGISTER_MEM=$MEM_30_PCT  VORTEX_MEM=$MEM_00_PCT  TRAEFIK_MEM=$MEM_05_PCT   REDIRECT_MEM=$MEM_05_PCT  \
+        BROWSER_CPU=$CPU_50_PCT  BIOREGISTER_CPU=$CPU_30_PCT  VORTEX_CPU=$CPU_00_PCT  TRAEFIK_CPU=$CPU_05_PCT   REDIRECT_MEM=$MEM_05_PCT   STACK_NAME=$BRO_STACK_NAME ENV=$ENV docker stack deploy --with-registry-auth  -c docker/stack/browser/browser.yml -c docker/stack/browser/browser-$ENV.yml $BRO_STACK_NAME
+
 
     elif [ "$BIOREGISTER_ENABLE" = "false" ] &&  [ "$VORTEX_ENABLE" = "true" ]  ; then
-        BROWSER_LIMIT_MEM=$MEM_70_PCT  BIOREGISTER_LIMIT_MEM=$MEM_00_PCT  VORTEX_LIMIT_MEM=$MEM_10_PCT  TRAEFIK_LIMIT_MEM=$MEM_05_PCT   REDIRECT_LIMIT_MEM=$MEM_05_PCT    STACK_NAME=$BRO_STACK_NAME ENV=$ENV docker stack deploy --with-registry-auth  -c docker/stack/browser/browser.yml -c docker/stack/browser/browser-$ENV.yml $BRO_STACK_NAME
+        BROWSER_MEM=$MEM_70_PCT  BIOREGISTER_MEM=$MEM_00_PCT  VORTEX_MEM=$MEM_10_PCT  TRAEFIK_MEM=$MEM_05_PCT   REDIRECT_MEM=$MEM_05_PCT  \
+        BROWSER_CPU=$CPU_70_PCT  BIOREGISTER_CPU=$CPU_00_PCT  VORTEX_CPU=$CPU_10_PCT  TRAEFIK_CPU=$CPU_05_PCT   REDIRECT_MEM=$MEM_05_PCT   STACK_NAME=$BRO_STACK_NAME ENV=$ENV docker stack deploy --with-registry-auth  -c docker/stack/browser/browser.yml -c docker/stack/browser/browser-$ENV.yml $BRO_STACK_NAME
+
     else
-        BROWSER_LIMIT_MEM=$MEM_80_PCT  BIOREGISTER_LIMIT_MEM=$MEM_00_PCT  VORTEX_LIMIT_MEM=$MEM_00_PCT  TRAEFIK_LIMIT_MEM=$MEM_05_PCT   REDIRECT_LIMIT_MEM=$MEM_05_PCT    STACK_NAME=$BRO_STACK_NAME ENV=$ENV docker stack deploy --with-registry-auth  -c docker/stack/browser/browser.yml -c docker/stack/browser/browser-$ENV.yml $BRO_STACK_NAME
+        BROWSER_MEM=$MEM_80_PCT  BIOREGISTER_MEM=$MEM_00_PCT  VORTEX_MEM=$MEM_00_PCT  TRAEFIK_MEM=$MEM_05_PCT   REDIRECT_MEM=$MEM_05_PCT  \
+        BROWSER_CPU=$CPU_80_PCT  BIOREGISTER_CPU=$CPU_00_PCT  VORTEX_CPU=$CPU_00_PCT  TRAEFIK_CPU=$CPU_05_PCT   REDIRECT_MEM=$MEM_05_PCT   STACK_NAME=$BRO_STACK_NAME ENV=$ENV docker stack deploy --with-registry-auth  -c docker/stack/browser/browser.yml -c docker/stack/browser/browser-$ENV.yml $BRO_STACK_NAME
+
         make ENV=$ENV scale_down_vortex
         make ENV=$ENV scale_down_bioregister
     fi
@@ -45,6 +53,7 @@ function set_up_configuration(){
     export MEM_30_PCT=$(grep MemTotal /proc/meminfo | awk '{print $2 / 1024*30/100"m"}' )
     export MEM_20_PCT=$(grep MemTotal /proc/meminfo | awk '{print $2 / 1024*20/100"m"}' )
     export MEM_10_PCT=$(grep MemTotal /proc/meminfo | awk '{print $2 / 1024*10/100"m"}' )
+    export MEM_05_PCT=$(grep MemTotal /proc/meminfo | awk '{print $2 / 1024*5/100"m"}' )
     export MEM_00_PCT=$(grep MemTotal /proc/meminfo | awk '{print $2 / 1024*0/100"m"}' )
 
 
@@ -57,6 +66,7 @@ function set_up_configuration(){
     export CPU_30_PCT=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{ print $4 * (30/100) } ' )
     export CPU_20_PCT=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{ print $4 * (20/100) } ' )
     export CPU_10_PCT=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{ print $4 * (10/100) } ' )
+    export CPU_05_PCT=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{ print $4 * ( 5/100) } ' )
     export CPU_00_PCT=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{ print $4 * ( 0/100) } ' )
 
 grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}'
